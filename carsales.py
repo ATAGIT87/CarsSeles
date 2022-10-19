71668cae725cf5e4d92a03fd876e4cf43a72f05c
ata <al.tabatabaei@yahoo.com>
2022-10-20 01:52:38 +0200

    add insu and add car pages

--- carsales.py
+++ carsales.py
@@ -19,9 +19,9 @@ def main():
     cars = []
     conn = connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM TblCars")
+    cursor.execute(" select p.name,p.address,i.des,c.name,c.year,c.price from CarSeles.TblPersons p, CarSeles.TblCars c, CarSeles.TblInsurance i where p.Car_ID=c.ID and p.Insurance_Id=i.ID;")
     for row in cursor.fetchall():
-        cars.append({"id": row[0], "name": row[1], "year": row[2], "price": row[3]})
+        cars.append({"name": row[0], "address": row[1], "des": row[2], "carname": row[3], "year": row[4], "price": row[5]})
     conn.close()
     return render_template("carslist.html", cars = cars)
 
@@ -40,6 +40,40 @@ def addcar():
         conn.commit()
         conn.close()
         return redirect('/')
+
+
+@carsales.route("/addinsu", methods = ['GET','POST'])
+def addinsu():
+    if request.method == 'GET':
+        return render_template("addinsu.html", insu = {})
+    if request.method == 'POST':
+        id = int(request.form["id"])
+        des = request.form["des"]
+        conn = connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO TblInsurance (id, des) VALUES (?, ?)", (id, des) )
+        conn.commit()
+        conn.close()
+        return redirect('/')
+
+
+
+@carsales.route("/addperson", methods = ['GET','POST'])
+def addperson():
+    if request.method == 'GET':
+        return render_template("addperson.html", perso = {})
+    if request.method == 'POST':
+        id = int(request.form["id"])
+        name = request.form["name"]
+        address = request.form["address"]
+        insu = int(request.form["insu"])
+        car = int(request.form["car"])
+        conn = connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO TblPersons (id,name,address,insurance_id,car_id) VALUES (?, ?,?,?,?)", (id, name,address,insu,car) )
+        conn.commit()
+        conn.close()
+        return redirect('/')        
 
 @carsales.route('/updatecar/<int:id>',methods = ['GET','POST'])
 def updatecar(id):
