197f71bd14510b16946ec5448d55d6a9e582f6cb
ATAGIT87 <116157751+ATAGIT87@users.noreply.github.com>
2023-02-07 23:29:47 +0100

    edit cars list and table
    I replaced the correct query and the insurance info should be displayed in the second table, but it doesn't work at the moment

--- app/app.py
+++ app/app.py
@@ -25,19 +25,19 @@ def index():
 def ajaxlivesearch():
      connection = db_connection()
      cursor = connection.cursor()
-     cars = []
+     person = []
      print('hiier ist ajax')
      if request.method == 'POST':
         search_word = request.form['query']
      if search_word == '':
             query = " select p.id,p.name,p.address,c.id,c.name,c.year,c.price,h.address,b.color,b.engine,j.titel from persons p, cars c,houses h, boats b, jobs j where p.car_id=c.id and p.house_id=h.id and p.boat_id=b.id and p.job_id=j.id ORDER BY p.name"
             cursor.execute(query)
-            cars= cursor.fetchall()
+            person= cursor.fetchall()
      else:    
             query = "select p.id,p.name,p.address,c.id,c.name,c.year,c.price,h.address,b.color,b.engine,j.titel from persons p, cars c,houses h, boats b, jobs j where p.car_id=c.id and p.house_id=h.id and p.boat_id=b.id and p.job_id=j.id and (p.name LIKE '%{}%' OR c.year LIKE '%{}%' OR c.price LIKE '%{}%') ORDER BY p.name DESC LIMIT 20".format(search_word,search_word,search_word)
             cursor.execute(query)
-            cars= cursor.fetchall()
-     return jsonify({'htmlresponse': render_template('response.html', cars=cars)})
+            person= cursor.fetchall()
+     return jsonify({'htmlresponse': render_template('response.html', person=person)})
 
 
 
@@ -47,8 +47,8 @@ def selectcars():
      cur = connection.cursor()
      cars = []
      print('hiier ist select cars')
-     search_word = request.form['id']
-     qu  = "select p.id,p.name,p.address,c.id,c.name,c.year,c.price,h.address,b.color,b.engine,j.titel from persons p, cars c,houses h, boats b, jobs j where p.car_id=c.id and p.house_id=h.id and p.boat_id=b.id and p.job_id=j.id and c.id LIKE '%{}%' ORDER BY p.name DESC LIMIT 20".format(search_word)
+     search_word = request.form['id'] 
+     qu  = "select c.id,c.name,c.year,c.price,i.id,i.des from cars c,insurance i where c.insu_id=i.id and c.id=%{}%".format(search_word)
      cur.execute(qu)
      cars= cur.fetchall()
      return jsonify({'htmlresponse': render_template('response.html', cars=cars)})
