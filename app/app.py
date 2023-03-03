8b80ac3942886eeb711a9edacaddb1ddcaf46b18
ATAGIT87 <116157751+ATAGIT87@users.noreply.github.com>
2023-03-03 17:50:40 +0100

    change live search method

--- app/app.py
+++ app/app.py
@@ -26,32 +26,17 @@ def ajaxlivesearch():
      connection = db_connection()
      cursor = connection.cursor()
      person = []
-     insu =[]
-     jobs =[]
-     print('hiier ist ajax')
      if request.method == 'POST':
         search_word = request.form['query'] 
      if search_word == '':
             query = " select p.id,p.name,p.address,c.id,c.name,c.year,c.price,h.address,b.color,b.engine,j.titel from persons p, cars c,houses h, boats b, jobs j where p.car_id=c.id and p.house_id=h.id and p.boat_id=b.id and p.job_id=j.id ORDER BY p.name"
-            queryinsu = " select id,des from insurance"
-            queryjobs = " select id, titel from jobs"
             cursor.execute(query)
             person= cursor.fetchall()
-            cursor.execute(queryinsu)
-            insu= cursor.fetchall()
-            cursor.execute(queryjobs)
-            jobs= cursor.fetchall()
      else:    
-            query = "select p.id,p.name,p.address,c.id,c.name,c.year,c.price,h.address,b.color,b.engine,j.titel from persons p, cars c,houses h, boats b, jobs j where p.car_id=c.id and p.house_id=h.id and p.boat_id=b.id and p.job_id=j.id and (p.name LIKE '%{}%' OR c.year LIKE '%{}%' OR c.price LIKE '%{}%') ORDER BY p.name DESC LIMIT 20".format(search_word,search_word,search_word)
+            query = "select p.id,p.name,p.address,c.id,c.name,c.year,c.price,h.address,b.color,b.engine,j.titel from persons p, cars c,houses h, boats b, jobs j where p.car_id=c.id and p.house_id=h.id and p.boat_id=b.id and p.job_id=j.id and (p.name LIKE '%{}%' OR c.name LIKE '%{}%' OR b.engine LIKE '%{}%') ORDER BY p.name DESC LIMIT 20".format(search_word,search_word,search_word)
             cursor.execute(query)
             person= cursor.fetchall()
-            queryinsu = " select id,des from insurance"
-            cursor.execute(queryinsu)
-            insu= cursor.fetchall()
-            queryjobs = " select id, titel from jobs"
-            cursor.execute(queryjobs)
-            jobs= cursor.fetchall()
-     return jsonify({'htmlresponse': render_template('response.html', person=person,insu=insu,jobs=jobs)})
+     return jsonify({'htmlperson': render_template('person.html', person=person)})
 
 
 
@@ -60,7 +45,6 @@ def selectcars():
      connection = db_connection()
      cur = connection.cursor()
      insu = []
-     print('hiier ist select cars')
      search_word = request.form['query2'] 
      if search_word == '':
         qu  = "select c.id,c.name,c.year,c.price,i.id,i.des from cars c,insurance i where c.insu_id=i.id"
@@ -71,7 +55,7 @@ def selectcars():
         cur.execute(qu)
         insu= cur.fetchall()  
     
-     return jsonify({'htmlresponse2': render_template('response2.html',insu=insu)})
+     return jsonify({'htmlcar': render_template('car.html',insu=insu)})
 
 @carsales.route("/selectinsu",methods=["POST","GET"])
 def selectinsu():
@@ -89,7 +73,7 @@ def selectinsu():
         cur.execute(qu)
         insu= cur.fetchall()  
      
-     return jsonify({'htmlresponse3': render_template('response3.html',insu=insu)})
+     return jsonify({'htmlinsu': render_template('insu.html',insu=insu)})
 
 if __name__ == '__main__':
     carsales.run(host='0.0.0.0')
